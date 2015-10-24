f9ef448be12cf4d853b72e513a07503bdb18b863
mzhang32 <mzhang32@gmail.com>
2015-10-24 12:19:46 -0700

    databases
    ...

--- ideaCollab.js
+++ ideaCollab.js
@@ -1,9 +1,19 @@
-var myDataRef = new Firebase('https://pr9laseml9o.firebaseio-demo.com/');
-      $('#messageInput').keypress(function (e) {
-        if (e.keyCode == 13) {
-          var name = $('#nameInput').val();
-          var text = $('#messageInput').val();
-          //[ADD SET() HERE]
-          $('#messageInput').val('');
-        }
-      });
+var myDataRef = new Firebase('https://glowing-heat-5533.firebaseio.com/');
+
+$('#messageInput').keypress(function (e) {
+    console.log("What a pain");
+    if (e.keyCode == 13) {
+    	console.log("Enter key");
+        var name = $('#nameInput').val();
+        var text = $('#messageInput').val();
+        myDataRef.set('User ' + name + ' says ' + text);
+        // Get a database reference to our posts
+		// Attach an asynchronous callback to read the data at our posts reference
+		myDataRef.on("value", function(snapshot) {
+		    console.log(snapshot.val());
+		}, function (errorObject) {
+		    console.log("The read failed: " + errorObject.code);
+		});
+        //$('#messageInput').val('');
+    }
+});
